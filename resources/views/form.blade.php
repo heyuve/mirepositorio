7f5c2b9c3a728744ba66f2c80fb247c4358681d5
heyuve <uvemint@gmail.com>
2021-01-25 09:32:58 +0100

    Añadir form

---
+++ resources/views/form.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+
+    <h3>Contact us</h3>
+
+<form action="">
+	
+
+<input type="text" name="nombre" placeholder="Nombre"> <br>
+
+<input type="text" name="apellido" placeholder="Apellido"> <br>
+
+<input type="email" name="correo" placeholder="Email"> <br>
+
+<input type="number" name="tel" placeholder="Teléfono"> <br>
+
+<textarea id="msg" placeholder="Envia tu mensaje"></textarea>
+
+
+<div>
+<button type="submit">Enviar</button>
+</div>
+
+
+
+
+</form>
+
+
+
+
+
+
+
+
+    
+</body>
+</html>
